52708d2c1f8f249db75a4a8da12659936568eccb
Neeraj <99793365+neeraj572@users.noreply.github.com>
2024-05-28 15:17:46 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#Email details
+receiver="h.anand@cgi.com"
+subject="Housekeeping script result"
+body_file="$output_file"
+#send email
+
+echo -e "$body_file" |mail -s "$subject" "$receiver" << EOF
+$(cat "$body_file")
+EOF
